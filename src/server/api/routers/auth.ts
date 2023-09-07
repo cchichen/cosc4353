e6d31f155de145445ab1810a817680ba98eb37c3
cchichen <oscers1001dennis@gmail.com>
2023-09-06 23:55:08 -0500

    Update auth.ts
    testing, setting samesite to false and secure to true

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -114,8 +114,8 @@ export const authRouter = createTRPCRouter({
       cookies.set("auth-session-id", sessionToken, {
         expires: sessionexpires,
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax', 
+        secure: true,
+        sameSite: 'false', 
       });
     } catch (e) {
       console.log("ERROR", e);
